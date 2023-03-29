80cdedd838189944668aa6d736e0fe1102c7e6e0
SteAnoe <ste.anoe@gmail.com>
2023-03-29 15:21:43 +0200

    Finito

--- main.js
+++ main.js
@@ -1,12 +1,31 @@
 let listaSpesa = [
     'pane',
-    'pane2',
-    'pane3'
-];
+    'formaggio',
+    'insalata'
+]
 
-let i = 0;
+let lista = document.querySelector('.lista');
+
+let i = 0
 
 while (i < listaSpesa.length) {
-    console.log(listaSpesa[i]);
+    let li = document.createElement('li');
+    lista.appendChild(li);
+    li.textContent = listaSpesa[i];
     i++;
 }
+
+
+function insert() {
+    let item = document.querySelector('#item').value;
+    let li = document.createElement('li');
+    listaSpesa.push(item);
+
+    while (i < listaSpesa.length) {
+    let li = document.createElement('li');
+    lista.appendChild(li);
+    li.textContent = item;
+    i++;  
+}
+} 
+
